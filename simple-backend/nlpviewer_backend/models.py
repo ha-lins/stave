576687a0161b81a0c95e6a022ea9396547acde22
Shuai Lin <shuailin97@gmail.com>
2020-06-21 22:27:15 +0800

    Update models.py

--- simple-backend/nlpviewer_backend/models.py
+++ simple-backend/nlpviewer_backend/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
+class Project(models.Model):
+    pro_name = models.CharField(max_length=200)
+    ontology = models.TextField(default='')
+
 
 class Document(models.Model):
     name = models.CharField(max_length=200)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        default=''
+    )
     textPack = models.TextField()
-    ontology = models.TextField()
 
 
 class User(models.Model):
